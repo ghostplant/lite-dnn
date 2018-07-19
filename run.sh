6b0cd8401e961abc4d9137c770a9b571ecf8c31f
CUI Wei <ghostplant@qq.com>
2018-07-19 09:11:41 -0400

    add image_generator

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@
 
 g++ -O3 -o $(dirname $0)/lite-model -std=c++14 $(dirname $0)/lite-model.cc \
     -I $(dirname $0)/lite-dnn -I/usr/local/cuda/include -L/usr/local/cuda/lib64 \
+    -lopencv_core -lopencv_highgui -lopencv_imgproc \
     -lcuda -lcudart -lcublas -lcudnn -lpthread
 
-LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH exec $(dirname $0)/lite-model "$@"
+LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH $(dirname $0)/lite-model "$@"
