5ccbf270af8298e1ddd91eee373818d8ebdc8137
ghostplant <12099308+ghostplant@users.noreply.github.com>
2018-07-22 01:38:31 -0400

    in-thread data generator (not finished)

--- lite-model.cc
+++ lite-model.cc
@@ -34,13 +34,6 @@
 using namespace std;
 
 
-#define MNIST_IMAGES "/tmp/mnist-images-idx3-ubyte"
-#define MNIST_LABELS "/tmp/mnist-labels-idx1-ubyte"
-
-#define CIFAR10_IMAGES "/tmp/cifar10-images-idx4-ubyte"
-#define CIFAR10_LABELS "/tmp/cifar10-labels-idx1-ubyte"
-
-
 vector<shared_ptr<Layer>> create_model(const char *model, int n_class) {
   vector<shared_ptr<Layer>> layers;
   if (!strcmp(model, "mnist_mlp")) {
@@ -140,7 +133,7 @@ vector<shared_ptr<Layer>> create_model(const char *model, int n_class) {
     layers.push_back(make_shared<Dense>(n_class));
     layers.push_back(make_shared<Softmax>());
   } else {
-    fprintf(stderr, "No model of name %s found.\n", model);
+    printf("No model of name %s found.\n", model);
     exit(1);
   }
   return move(layers);
@@ -158,49 +151,158 @@ static inline unsigned long get_microseconds() {
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <unordered_map>
+#include <queue>
 
 using std::unordered_map;
 
-auto image_generator(string path, int height = 229, int width = 229) {
-
-  if (path.size() > 0 && path[path.size() - 1] != '/')
-    path += '/';
+auto image_generator(string path, int height = 229, int width = 229, int cache_size = 256) {
 
   struct Generator {
     unordered_map<string, vector<string>> dict;
     vector<string> keyset;
     int n_class, channel, height, width;
+    queue<vector<float>> q_chw, q_l;
+
+    int cache_size;
+    vector<pthread_t> tids;
+    pthread_mutex_t m_lock;
+    bool thread_stop;
+    unordered_map<string, vector<vector<float>>> cached;
+
+    Generator(const string &path, int height, int width, int cache_size): height(height), width(width), cache_size(cache_size), channel(3) {
+      pthread_mutex_init(&m_lock, 0);
+      thread_stop = 0;
+
+      dirent *ep, *ch_ep;
+      DIR *root = opendir(path.c_str());
+      assert(root != nullptr);
+
+      while ((ep = readdir(root)) != nullptr) {
+        if (!ep->d_name[1] || (ep->d_name[1] == '.' && !ep->d_name[2]))
+          continue;
+        string sub_dir = path + ep->d_name + "/";
+        DIR *child = opendir(sub_dir.c_str());
+        if (child == nullptr)
+          continue;
+        while ((ch_ep = readdir(child)) != nullptr) {
+          if (!ch_ep->d_name[1] || (ch_ep->d_name[1] == '.' && !ch_ep->d_name[2]))
+            continue;
+          dict[sub_dir].push_back(ch_ep->d_name);
+        }
+        closedir(child);
+      }
+      closedir(root);
+
+      keyset.clear();
+      int samples = 0;
+      for (auto &it: dict) {
+        keyset.push_back(it.first);
+        samples += it.second.size();
+      }
+      n_class = keyset.size();
+
+      tids.resize(1);
+      for (int i = 0; i < tids.size(); ++i)
+        assert(!pthread_create(&tids[i], NULL, Generator::start, this));
+
+      printf("Total %d samples found with %d classes.\n", samples, n_class);
+
+      /*long x = time(0);
+      for (int i = 0; i < keyset.size(); ++i) {
+        for (auto &it: dict[keyset[i]]) {
+          vector<float> chw(channel * height * width), l(n_class, 0.0f);
+          string path = keyset[i] + it;
+          bool ans = get_image_data(path, chw.data(), i, l.data());
+
+          pthread_mutex_lock(&m_lock);
+          if (ans)
+            cached[path] = {move(chw), move(l)};
+          else
+            cached[path] = {};
+          pthread_mutex_unlock(&m_lock);
+        }
+      }
+      long y = time(0);
+      printf("Done %zd\n", y - x);
+      exit(0);*/
+    }
+
+    ~Generator() {
+      void *ret;
+      thread_stop = 1;
+      for (auto tid: tids)
+        pthread_join(tid, &ret);
+      pthread_mutex_destroy(&m_lock);
+    }
+
+
+    bool get_image_data(const string &image_path, float *chw, int one_hot, float *l) {
+      cv::Mat image = cv::imread(image_path, 1);
+      if (image.data == nullptr)
+        return false;
+
+      cv::Size dst_size(height, width);
+      cv::Mat dst;
+      cv::resize(image, dst, dst_size);
+      l[one_hot] = 1.0f;
+
+      uint8_t *ptr = dst.data;
+      float *b = chw, *g = b + height * width, *r = g + height * width;
+      for (int h = 0; h < height; ++h) {
+        for (int w = 0; w < width; ++w) {
+          *b++ = *ptr++ / 255.0f, *g++ = *ptr++ / 255.0f, *r++ = *ptr++ / 255.0f;
+        }
+      }
+      // cv::imwrite("/tmp/image-0.jpg", dst);
+      return true;
+    }
+
+    void background_generator() {
+      while (!thread_stop) {
+        while (!thread_stop) {
+          pthread_mutex_lock(&m_lock);
+          if (q_chw.size() >= cache_size) {
+            pthread_mutex_unlock(&m_lock);
+            continue;
+          } else
+            break;
+        }
+        if (thread_stop)
+          continue;
+
+        int c = rand() % dict.size();
+        auto &files = dict[keyset[c]];
+        int it = rand() % files.size();
+
+        // auto &data = cached[keyset[c] + files[it]]; if (data.size() > 0) q_chw.push(data[0]), q_l.push(data[1]);
+        vector<float> chw(channel * height * width), l(n_class, 0.0f); if (get_image_data(keyset[c] + files[it], chw.data(), c, l.data())) q_chw.push(move(chw)), q_l.push(move(l));
+
+        pthread_mutex_unlock(&m_lock);
+      }
+    }
+
+    static void *start(void *arg) {
+      ((Generator*)arg)->background_generator();
+      return NULL;
+    }
 
     auto next_batch(int batch_size = 32) {
       vector<float> nchw(batch_size * channel * height * width);
       vector<float> nl(batch_size * keyset.size());
 
-      for (int i = 0; i < batch_size; ++i) {
-        int c = rand() % dict.size();
-        auto &files = dict[keyset[c]];
-        int it = rand() % files.size();
-
-        string image_path = keyset[c] + files[it];
-        cv::Mat image = cv::imread(image_path, 1);
-        cv::Size dst_size(height, width);
-        cv::Mat dst;
-        if (image.data == nullptr) {
-          --i;
-          continue;
+      int it = 0;
+      while (it < batch_size) {
+        pthread_mutex_lock(&m_lock);
+        while (it < batch_size && q_chw.size()) {
+          float *images = nchw.data() + (channel * height * width) * it;
+          float *labels = nl.data() + (n_class) * it;
+          memcpy(images, q_chw.front().data(), (channel * height * width) * sizeof(float));
+          memcpy(labels, q_l.front().data(), (n_class) * sizeof(float));
+          q_chw.pop();
+          q_l.pop();
+          ++it;
         }
-        cv::resize(image, dst, dst_size);
-
-        nl[keyset.size() * i + c] = 1.0f; // one hot
-
-        uint8_t *ptr = dst.data;
-        float *b = nchw.data() + (channel * height * width) * i;
-        float *g = b + height * width, *r = g + height * width;
-        for (int h = 0; h < height; ++h) {
-          for (int w = 0; w < width; ++w) {
-            *b++ = *ptr++ / 255.0f, *g++ = *ptr++ / 255.0f, *r++ = *ptr++ / 255.0f;
-          }
-        }
-        // cv::imwrite("/var/www/html/0-resize.jpg", dst);
+        pthread_mutex_unlock(&m_lock);
       }
 
       struct dataset {
@@ -212,39 +314,11 @@ auto image_generator(string path, int height = 229, int width = 229) {
         Tensor({batch_size, (int)keyset.size()}, nl.data())
       });
     }
-  } gen;
+  };
 
-  dirent *ep, *ch_ep;
-  DIR *root = opendir(path.c_str());
-  assert(root != nullptr);
-
-  while ((ep = readdir(root)) != nullptr) {
-    if (!ep->d_name[1] || (ep->d_name[1] == '.' && !ep->d_name[2]))
-      continue;
-    string sub_dir = path + ep->d_name + "/";
-    DIR *child = opendir(sub_dir.c_str());
-    if (child == nullptr)
-      continue;
-    while ((ch_ep = readdir(child)) != nullptr) {
-      if (!ch_ep->d_name[1] || (ch_ep->d_name[1] == '.' && !ch_ep->d_name[2]))
-        continue;
-      gen.dict[sub_dir].push_back(ch_ep->d_name);
-    }
-    closedir(child);
-  }
-  closedir(root);
-
-  gen.keyset.clear();
-
-  int samples = 0;
-  for (auto &it: gen.dict) {
-    gen.keyset.push_back(it.first);
-    samples += it.second.size();
-  }
-  gen.n_class = gen.keyset.size(), gen.channel = 3, gen.height = height, gen.width = width;
-
-  printf("Total %d samples found with %d classes.\n", samples, gen.n_class);
-  return gen;
+  if (path.size() > 0 && path[path.size() - 1] != '/')
+    path += '/';
+  return make_unique<Generator>(path, height, width, cache_size);
 }
 
 auto array_generator(const char* images_ubyte, const char* labels_ubyte) {
@@ -285,23 +359,31 @@ auto array_generator(const char* images_ubyte, const char* labels_ubyte) {
         });
       }
     }
-  } gen;
+  };
+
+  auto gen = make_unique<Generator>();
 
   auto full_images = ReadNormalDataset(images_ubyte);
   auto full_labels = ReadNormalDataset(labels_ubyte);
   assert(full_images.first[0] == full_labels.first[0]);
 
-  gen.images_data = move(full_images.second);
-  gen.labels_data = move(full_labels.second);
+  gen->images_data = move(full_images.second);
+  gen->labels_data = move(full_labels.second);
 
-  gen.curr_iter = 0;
-  gen.n_sample = full_labels.first[0];
-  gen.n_class = full_labels.first[1], gen.channel = full_images.first[1], gen.height = full_images.first[2], gen.width = full_images.first[3];
+  gen->curr_iter = 0;
+  gen->n_sample = full_labels.first[0];
+  gen->n_class = full_labels.first[1], gen->channel = full_images.first[1], gen->height = full_images.first[2], gen->width = full_images.first[3];
 
-  printf("Total %d samples found with %d classes.\n", gen.n_sample, gen.n_class);
-  return gen;
+  printf("Total %d samples found with %d classes.\n", gen->n_sample, gen->n_class);
+  return move(gen);
 }
 
+
+#define MNIST_IMAGES "/tmp/mnist-images-idx3-ubyte"
+#define MNIST_LABELS "/tmp/mnist-labels-idx1-ubyte"
+
+#define CIFAR10_IMAGES "/tmp/cifar10-images-idx4-ubyte"
+#define CIFAR10_LABELS "/tmp/cifar10-labels-idx1-ubyte"
 
 int main(int argc, char **argv) {
   Tensor::init();
@@ -309,11 +391,10 @@ int main(int argc, char **argv) {
   // auto gen = image_generator("/docker/PetImages", 32, 32);
   auto gen = array_generator(MNIST_IMAGES, MNIST_LABELS);
 
+  int batch_size = 128, steps = 60000;
+  vector<int> shape = {batch_size, gen->channel, gen->height, gen->width};
 
-  int batch_size = 128, steps = 600000;
-  vector<int> shape = {batch_size, gen.channel, gen.height, gen.width};
-
-  auto model = create_model(argc > 1 ? argv[1] : "mnist_cnn", gen.n_class);
+  auto model = create_model(argc > 1 ? argv[1] : "mnist_cnn", gen->n_class);
   puts("");
   for (int i = 0; i < model.size(); ++i) {
     shape = model[i]->configure(shape);
@@ -328,7 +409,7 @@ int main(int argc, char **argv) {
   static unsigned long lastClock = get_microseconds();
 
   for (int k = 0, it = 0; k < steps; ++k) {
-    auto batch = gen.next_batch(batch_size); auto &images = batch.images, &labels = batch.labels;
+    auto batch = gen->next_batch(batch_size); auto &images = batch.images, &labels = batch.labels;
 
     float lr = - float(0.05f * pow((1.0f + 0.0001f * k), -0.75f));
 
@@ -344,7 +425,7 @@ int main(int argc, char **argv) {
 
     unsigned long currClock = get_microseconds();
     if (currClock >= lastClock + 1000000) {
-      printf("step = %d: loss = %.4f, acc = %.2f%%, time = %.4fs\n", k, get_loss(data_loss), get_accuracy(data_output, labels), (currClock - lastClock) * 1e-6f);
+      printf("step = %d: lr = %.4f, loss = %.4f, acc = %.2f%%, time = %.4fs\n", k, lr, get_loss(data_loss), get_accuracy(data_output, labels), (currClock - lastClock) * 1e-6f);
       lastClock = currClock;
     }
   }
