0a984cf518c1b45a57415fdb624559528dd5f49d
ghostplant <12099308+ghostplant@users.noreply.github.com>
2018-09-15 20:06:09 -0400

    enhance tf-model

--- tf-model.py
+++ tf-model.py
@@ -12,15 +12,6 @@ def u_rand(seed):
   seed = ((seed & 0x7fffffff) * 1103515245 + 12345) & 0x7fffffff
   return seed
 
-batch_size = 64
-steps = 50000
-
-image_shapes = (None, 3, 224, 224)
-label_shapes = (None, 2)
-
-place_X = tf.placeholder(tf.float32, list(image_shapes))
-place_Y = tf.placeholder(tf.float32, list(label_shapes))
-
 def gen_fixed_random(shape, limit):
   # Fill random data that matches lite-model
   limit = limit * 2 / float(0x7fffffff)
@@ -45,13 +36,6 @@ def gen_fixed_random(shape, limit):
             new_val[i0][i1][i2][i3] = val[i0 * stride[0] + i1 * stride[1] + i2 * stride[2] + i3 * stride[3]]
     val = new_val
   return val
-
-def energy(val):
-  val = val.reshape([-1])
-  tot = 0
-  for i in range(len(val)):
-    tot += val[i] * val[i]
-  return tot
 
 def init_weight(shape):
   if len(shape) == 2:
@@ -84,7 +68,7 @@ def flatten(X):
   return tf.reshape(X, shape=[-1, np.product(X.shape[1:])])
 
 
-def create_imagenet_alexnet(X):
+def create_imagenet_alexnet(X, n_classes):
   print('Creating imagenet_alexnet ..')
   X = conv2d(X, 96, 11, 4, 2)
   X = tf.nn.relu(X)
@@ -109,11 +93,11 @@ def create_imagenet_alexnet(X):
   X = dense(X, 4096)
   X = tf.nn.relu(X)
   X = tf.nn.dropout(X, 0.25)
-  X = dense(X, label_shapes[1])
+  X = dense(X, n_classes)
   return X
 
 
-def create_imagenet_resnet50v1(X):
+def create_imagenet_resnet50v1(X, n_classes):
   print('Creating imagenet_resnet50v1 ..')
   assert(int(X.shape[2]) == 224 and int(X.shape[3]) == 224 and int(X.shape[1]) == 3)
   X = conv2d(X, 64, 7, 2, 3)
@@ -149,42 +133,72 @@ def create_imagenet_resnet50v1(X):
 
   X = apool2d(X, int(X.shape[2]), 1)
   X = flatten(X)
-  X = dense(X, label_shapes[1])
+  X = dense(X, n_classes)
   return X
+
+
+def create_dataset(data_dir, batch_size=64, height=224, width=224):
+  # Prepare Dataset
+  from keras.preprocessing.image import ImageDataGenerator
+  from keras.utils import OrderedEnqueuer
+
+  gen = ImageDataGenerator(data_format='channels_first', rescale=1./255, fill_mode='nearest').flow_from_directory(
+                           data_dir + '/train', target_size=(height, width), batch_size=batch_size)
+  val_gen = ImageDataGenerator(data_format='channels_first', rescale=1./255).flow_from_directory(
+                               data_dir + '/validate', target_size=(height, width), batch_size=batch_size)
+
+  if gen.num_classes != val_gen.num_classes:
+    raise Exception("The number of train classes and validate classes must be equal.")
+
+  enqueuer = OrderedEnqueuer(gen, use_multiprocessing=False)
+  enqueuer.start(workers=8)
+  val_enqueuer = OrderedEnqueuer(gen, use_multiprocessing=False)
+  val_enqueuer.start(workers=1)
+  return enqueuer, val_enqueuer, gen.num_classes, (3, height, width)
 
 
 config = tf.ConfigProto()
 config.gpu_options.allow_growth=True
 
+batch_size, steps = 64, 10000
+# data_dir = '/var/lib/docker/imagenet'
+data_dir = '/tmp/dataset/catsdogs'
+
 with tf.Session(config=config) as sess:
-  # X = create_imagenet_alexnet(place_X)
-  X = create_imagenet_resnet50v1(place_X)
+  enqueuer, val_enqueuer, n_classes, image_shape = create_dataset(data_dir=data_dir, batch_size=batch_size)
+  place_X = tf.placeholder(tf.float32, (None,) + image_shape)
+  place_Y = tf.placeholder(tf.float32, (None, n_classes))
+
+  # X = create_imagenet_alexnet(place_X, n_classes)
+  X = create_imagenet_resnet50v1(place_X, n_classes)
 
   loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=X, labels=place_Y))
   accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(X, 1), tf.argmax(place_Y, 1)), tf.float32))
-  optimizer = tf.train.MomentumOptimizer(learning_rate=0.01, momentum=0.9).minimize(loss)
+  optimizer = tf.train.MomentumOptimizer(learning_rate=0.001, momentum=0.9).minimize(loss)
 
   sess.run(tf.global_variables_initializer())
 
-  from keras.preprocessing.image import ImageDataGenerator
-  from keras.utils import OrderedEnqueuer
-  gen = ImageDataGenerator(
-        data_format='channels_first',
-        rescale=1./255,
-        fill_mode='nearest').flow_from_directory('/tmp/dataset/catsdogs/train', target_size=image_shapes[2:], batch_size=batch_size)
-  enqueuer = OrderedEnqueuer(gen, use_multiprocessing=True)
-  enqueuer.start(workers=4)
+  session_ckpt = "/tmp/model.ckpt"
+  saver = tf.train.Saver()
+  try:
+    saver.restore(sess, session_ckpt + "/session")
+    print('Restore last session.')
+  except:
+    print('Create new session.')
 
+  # Start Training
   init_time = last_time = time.time()
-  k = 1
-  for batch_xs, batch_ys in enqueuer.get():
+  for k in range(1, steps + 1):
+    batch_xs, batch_ys = next(enqueuer.get())
     sess.run(optimizer, feed_dict={place_X: batch_xs, place_Y: batch_ys})
     curr_time = time.time()
-    if k % 100 == 0:
-    # if curr_time >= last_time + 1.0:
+    if k % 100 == 0 or k == 1:
+      val_xs, val_ys = next(val_enqueuer.get())
       out_loss, out_acc = sess.run([loss, accuracy], feed_dict={place_X: batch_xs, place_Y: batch_ys})
-      print('step = %d (batch = %d; %.2f images/sec): loss = %.4f, acc = %.1f%%, time = %.3fs' % (k, batch_size, batch_size * (k + 1) / (curr_time - init_time), out_loss, out_acc *1e2, curr_time - last_time))
+      val_loss, val_acc = sess.run([loss, accuracy], feed_dict={place_X: val_xs, place_Y: val_ys})
+      print('step = %d (batch = %d; %.2f images/sec): loss = %.4f, acc = %.1f%%, val_loss = %.4f, val_acc = %.1f%%, time = %.3fs' %
+        (k, batch_size, batch_size * k / (curr_time - init_time), out_loss, out_acc * 1e2, val_loss, val_acc * 1e2, curr_time - last_time))
       last_time = curr_time
-    k = k + 1
-    if k > steps:
-      break
+    if k % 1000 == 0 or k == steps:
+      save_path = saver.save(sess, session_ckpt + "/session")
+      print("Model saved in path: %s" % save_path)
