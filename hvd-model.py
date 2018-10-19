311929bfd3ee4cbe436da3f56590e3a96b7aa8c8
ghostplant <12099308+ghostplant@users.noreply.github.com>
2018-10-19 23:21:42 +0800

    add backend generator

--- hvd-model.py
+++ hvd-model.py
@@ -3,51 +3,54 @@
 [Example]
 
 # HOROVOD_GPU_ALLREDUCE=NCCL pip3 install --no-cache-dir --upgrade horovod
-# g++ main.cpp -fPIC -shared -lpthread -lcuda -lcudart -I/usr/local/cuda/include -L/usr/local/cuda/lib64 && mpiexec -H 192.168.2.130,192.168.2.130,192.168.2.130,192.168.2.130,192.168.2.131,192.168.2.131,192.168.2.131,192.168.2.131,192.168.2.132,192.168.2.132,192.168.2.132,192.168.2.132 \
-    --mca oob_tcp_if_include enp216s0 --mca btl_tcp_if_include enp216s0 -x NCCL_SOCKET_IFNAME=enp216s0 \
-    --allow-run-as-root --map-by slot --bind-to none -x LD_PRELOAD=`pwd`/a.out -x NCCL_DEBUG=INFO ./hvd-model.py # /usr/imagenet/flower_photos
+# g++ generator.cpp -fPIC -O3 -std=c++14 -shared -lpthread -lcuda -lcudart -lopencv_core -lopencv_highgui -lopencv_imgproc -I/usr/local/cuda/include -L/usr/local/cuda/lib64 && mpiexec -H 192.168.2.130,192.168.2.130,192.168.2.130,192.168.2.130,192.168.2.131,192.168.2.131,192.168.2.131,192.168.2.131,192.168.2.132,192.168.2.132,192.168.2.132,192.168.2.132  --mca oob_tcp_if_include enp216s0 --mca btl_tcp_if_include enp216s0 -x NCCL_SOCKET_IFNAME=enp216s0     --allow-run-as-root --map-by slot --bind-to none -x LD_PRELOAD=`pwd`/a.out -x NCCL_DEBUG=INFO -x GPUAAS=1 ./hvd-model.py # /var/lib/docker/imagenet/train
 '''
+
+import time, os, sys
+
+batch_size, depth, height, width = 64, 3, 224, 224
+os.environ['GPUAAS_BATCHSIZE'] = str(batch_size)
+os.environ['GPUAAS_HEIGHT'] = str(height)
+os.environ['GPUAAS_WIDTH'] = str(width)
+os.environ['GPUAAS_BATCHSIZE'] = str(batch_size)
+os.environ['GPUAAS_FORMAT'] = 'NHWC'
+model_format = 'NHWC'
 
 import tensorflow as tf
 import numpy as np
-import time, os, sys
 import horovod.tensorflow as hvd
 
 hvd.init()
-device_name = 'GPU: %%%dd/%%d' % len(str(hvd.size())) % (hvd.rank() + 1, hvd.size())
+device_rank = hvd.rank()
+device_name = 'GPU: %%%dd/%%d' % len(str(hvd.size())) % (device_rank + 1, hvd.size())
 
-batch_size, depth, height, width = 64, 3, 224, 224
-global_step, query_freq = 100000, 100
+global_step, query_freq = 100000, 200
 
 
 def create_dataset(data_dir=None):
-  if not data_dir:
-    n_classes = 1001
-    if 'GPUAAS' not in os.environ:
-      print('Using synthetic dataset on %s..' % device_name)
-      X = tf.random_uniform((batch_size, depth, height, width), dtype=tf.float32, seed=1)
-      Y = tf.random_uniform((batch_size, ), maxval=n_classes, dtype=tf.int32, seed=2)
-      return X, Y, n_classes
+  if data_dir is not None and data_dir[-1] != '/':
+    data_dir += '/'
+  n_classes = 1001
 
+  if 'GPUAAS_DATASET' in os.environ:
     print('Using low-level dataset on %s..' % device_name)
     with tf.device('/cpu:0'):
       os.environ['IMAGE_SIZE'] = str(np.product([batch_size, depth, height, width]))
       os.environ['LABEL_SIZE'] = str(np.product([batch_size, ]))
-      ds = tf.data.Dataset.from_tensors((tf.fill((batch_size, depth, height, width), np.float32(np.NaN)), tf.fill((batch_size, ), np.int32(0x7fc00001)))).repeat()
+      ds = tf.data.Dataset.from_tensors((tf.fill((batch_size, depth, height, width), np.float32(np.NaN)), tf.fill((batch_size, ), np.int32(0x7fc00001))))
+      ds = ds.cache().repeat().prefetch(1024)
       images, labels = ds.make_one_shot_iterator().get_next()
       X = tf.reshape(images, (-1, depth, height, width))
       Y = tf.reshape(labels, (-1, ))
     return X, Y, n_classes
 
-  print('Using real dataset `%s` on %s..' % (data_dir, device_name))
+  if not data_dir:
+    print('Using synthetic dataset on %s..' % device_name)
+    X = tf.random_uniform((batch_size, depth, height, width), dtype=tf.float32, seed=1)
+    Y = tf.random_uniform((batch_size, ), maxval=n_classes, dtype=tf.int32, seed=2)
+    return X, Y, n_classes
 
-  if data_dir[-1] != '/':
-    data_dir += '/'
-  n_classes = 0
-  for subdir in os.listdir(data_dir):
-    path = os.path.join(data_dir, subdir)
-    if os.path.isdir(path):
-      n_classes = n_classes + 1
+  print('Using real dataset `%s` on %s..' % (data_dir, device_name))
 
   def generator():
     import warnings
@@ -59,7 +62,7 @@ def create_dataset(data_dir=None):
     from keras.utils import OrderedEnqueuer
     gen = ImageDataGenerator(data_format='channels_first', rescale=1./255, fill_mode='nearest').flow_from_directory(
                              data_dir, target_size=(height, width), batch_size=batch_size, class_mode='sparse')
-    assert(n_classes == gen.num_classes)
+    assert(gen.num_classes <= n_classes)
     enqueuer = OrderedEnqueuer(gen, use_multiprocessing=False)
     enqueuer.start(workers=8)
     while True:
@@ -68,7 +71,7 @@ def create_dataset(data_dir=None):
     enqueuer.close() # Never reached
 
   ds = tf.data.Dataset.from_generator(generator, (tf.float32, tf.int32))
-  ds = ds.prefetch(buffer_size=1024)
+  ds = ds.repeat().prefetch(buffer_size=1024)
   images, labels = ds.make_one_shot_iterator().get_next()
   X = tf.reshape(images, (-1, depth, height, width))
   Y = tf.reshape(labels, (-1,))
@@ -83,7 +86,7 @@ def create_imagenet_resnet50v1(X, Y, n_classes):
   # Using external models
   from models.resnet_model import create_resnet50_model
   model = create_resnet50_model()
-  X, _ = model.build_network(X, nclass=n_classes, image_depth=depth, data_format='NHWC', phase_train=True, fp16_vars=False)
+  X, _ = model.build_network(X, nclass=n_classes, image_depth=depth, data_format=model_format, phase_train=True, fp16_vars=False)
   loss = tf.losses.sparse_softmax_cross_entropy(logits=X, labels=Y)
   # accuracy = tf.reduce_mean(tf.cast(tf.nn.in_top_k(X, Y, 1), tf.float32))
   accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.cast(Y, tf.int64), tf.argmax(X, 1)), tf.float32))
@@ -91,7 +94,7 @@ def create_imagenet_resnet50v1(X, Y, n_classes):
 
 loss, accuracy, weights = create_imagenet_resnet50v1(X, Y, n_classes)
 
-lr = 0.01
+lr = 0.001
 # opt = tf.train.GradientDescentOptimizer(learning_rate=lr)
 # opt = tf.train.MomentumOptimizer(learning_rate=lr, momentum=0.9)
 # opt = tf.train.MomentumOptimizer(learning_rate=lr, momentum=0.9)
@@ -114,8 +117,27 @@ config=tf.ConfigProto(allow_soft_placement=True)
 config.gpu_options.allow_growth=True
 config.gpu_options.visible_device_list = str(hvd.local_rank())
 
-with tf.train.MonitoredTrainingSession(config=config, hooks=[hvd.BroadcastGlobalVariablesHook(0)]) as sess:
-  # sess.run(tf.global_variables_initializer())
+
+checkpoint_file = './resnet50-weights.npy'
+
+try:
+  weights_data = np.load(checkpoint_file)
+except Exception as e:
+  weights_data = None
+
+with tf.Session(config=config) as sess:
+  sess.run(tf.global_variables_initializer())
+  if device_rank == 0:
+    if weights_data is not None:
+      print('Try using pre-trained weights.')
+      assign_ops = []
+      for symbolic_weight, host_weight in zip(weights, weights_data):
+        assign_ops.append(tf.assign(symbolic_weight, host_weight))
+      sess.run(assign_ops)
+    else:
+      print('Not using pre-trained weights.')
+  sess.run(hvd.broadcast_global_variables(0))
+
   print('Launch Training on %s..' % device_name)
   last_time, last_k = time.time(), -1
   for k in range(global_step):
@@ -126,4 +148,9 @@ with tf.train.MonitoredTrainingSession(config=config, hooks=[hvd.BroadcastGlobal
       during = curr_time - last_time
       print('[%s] step = %d (batch = %d; %.2f images/sec): loss = %.4f, acc = %.2f%%, during = %.3fs' % (device_name, (k + 1),
         batch_size, batch_size * (k - last_k) / during, out_loss, out_acc * 1e2, during))
+      if (k + 1) % 5000 == 0 or (k + 1) == global_step:
+         weights_data = sess.run(weights)
+         if device_rank == 0:
+           print('Saving current weights on [%s]..' % device_name)
+           np.save(checkpoint_file, weights_data)
       last_time, last_k = time.time(), k
